f8c133b7e020835db78b2a00d61ef43d16ec25dc
mistusik <mistusik@gmail.com>
2019-12-13 15:33:00 +0100

    v.07

--- src/main/java/com/mst/library/app/Library.java
+++ src/main/java/com/mst/library/app/Library.java
@@ -4,19 +4,22 @@ import com.mst.library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
+        final String appName = "com.mst.library.app.Library v0.7";
 
+        Book[] books = new Book[1000];
 
-        final String appName = "com.mst.library.app.Library v0.5";
-
-        Book book1 = new Book("Core Java Volume 1 - Fundamentals", "Cay S. Horstman",
+        books[0] = new Book("Core Java Volume 1 - Fundamentals", "Cay S. Horstman",
                 2016, 872, "Pearson Education", "9788373271890");
-        Book book2 = new Book("Brave New World", "Aldous Huxley", 1932, 311,
+        books[1] = new Book("Brave New World", "Aldous Huxley", 1932, 311,
                 "Vintage Classics", "8798654456664");
+        books[2] = new Book("The Little Prince", "Antoine de Saint-Exupery", 1942, 100,
+                "Publisher1");
 
         System.out.println(appName);
         System.out.println("Available books");
-        book1.printInfo();
-        book2.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
 
 
     }
